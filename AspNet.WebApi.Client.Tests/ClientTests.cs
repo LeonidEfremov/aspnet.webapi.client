c863e7665ca5f97e4d0a1d18a4cc48b2abbfa921
Leonid Efremov <leonid.efremov@me.com>
2021-01-25 10:11:08 +0300

    net5.0 support

--- AspNet.WebApi.Client.Tests/ClientTests.cs
+++ AspNet.WebApi.Client.Tests/ClientTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using KellermanSoftware.CompareNetObjects;
 using Xunit;
 using Xunit.Asserts.Compare;
 
@@ -10,12 +11,15 @@ namespace AspNet.WebApi.Tests
     public class ClientTests
     {
         private readonly ClientBase _client;
+        private readonly CompareLogic _compareLogic;
 
         public ClientTests()
         {
             var configuration = new Configuration { Endpoint = new Uri("http://localhost") };
 
             _client = new Client(configuration) { SetCustomHeadersAction = SetHeadersAction };
+
+            _compareLogic = new CompareLogic();
         }
 
         private void SetHeadersAction(HttpRequestHeaders headers)
@@ -38,7 +42,7 @@ namespace AspNet.WebApi.Tests
         [Fact]
         public void ProductVersion()
         {
-            Assert.Equal("1.2.0.0", _client.ProductVersion);
+            Assert.Equal("1.3.0.0", _client.ProductVersion);
         }
 
         [Fact]
@@ -48,7 +52,7 @@ namespace AspNet.WebApi.Tests
 
             _client.PrepareRequest(httpClient, null, null);
 
-            Assert.Equal("AspNet.WebApi.Client/1.2.0.0", httpClient.DefaultRequestHeaders.UserAgent.Single().ToString());
+            Assert.Equal("AspNet.WebApi.Client/1.3.0.0", httpClient.DefaultRequestHeaders.UserAgent.Single().ToString());
             Assert.Equal("http://localhost/", httpClient.BaseAddress.ToString());
             Assert.Equal("Value", httpClient.DefaultRequestHeaders.GetValues("X-Custom-Header").SingleOrDefault());
         }
@@ -61,7 +65,7 @@ namespace AspNet.WebApi.Tests
 
             _client.ProcessResponse(httpClient, response);
 
-            DeepAssert.Equal(response, response);
+            Assert.True(_compareLogic.Compare(response, response).AreEqual);
         }
     }
 }
